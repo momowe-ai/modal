8284874334abb15031cd319254534d9d09035e3c
momowe-ai <dapaolulu@gmail.com>
2026-01-09 08:59:04 +0800

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -275,6 +275,7 @@ fastapi_app = FastAPI(lifespan=lifespan)
     secrets=[app_secrets],
     timeout=86400,
     keep_warm=1,
+    region="uk-london-1"
 )
 @modal.asgi_app()
 def web_server():
@@ -296,3 +297,4 @@ def web_server():
             return Response(content=f"读取订阅时发生错误: {e}", status_code=500, media_type="text/plain; charset=utf-8")
     
     return fastapi_app
+
