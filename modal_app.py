7b2f496c2fbad812415f8d50bccbfdf8a6a86635
momowe-ai <dapaolulu@gmail.com>
2026-05-02 10:15:32 +0800

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -275,7 +275,7 @@ fastapi_app = FastAPI(lifespan=lifespan)
     secrets=[app_secrets],
     timeout=86400,
     min_containers=1,
-    region="us-phoenix-1"
+    region="ap-melbourne-1"
 )
 @modal.asgi_app()
 def web_server():
